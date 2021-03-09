8cec8480fd0e2d6b740b381285c11c978b476971
wangshuang <wangshuang@ciics.com>
2021-03-09 19:38:05 +0800

    观察者模式

---
+++ src/main/java/Observer/Aunt.java
@@ -0,0 +1,8 @@
+package Observer;
+
+public class Aunt extends  AbstractAction {
+    @Override
+    public void action() {
+        System.out.println("阿姨卖饭");
+    }
+}
---
+++ src/main/java/Observer/Teacher.java
@@ -0,0 +1,8 @@
+package Observer;
+
+public class Teacher extends AbstractAction{
+    @Override
+    public void action() {
+        System.out.println("老师下课了");
+    }
+}
