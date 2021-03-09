b86269e9cd905f610f1c5e5f04c4a1460e4bd66b
wangshuang <wangshuang@ciics.com>
2021-03-09 20:26:34 +0800

    spring观察者模式

---
+++ src/main/java/SpringObserver/SpringObserverTest.java
@@ -0,0 +1,16 @@
+package SpringObserver;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * ApplicationContext(spring容器)在初始化完毕
+ * 通知
+ */
+public class SpringObserverTest {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("SpringObserverApplicationContext.xml");
+
+    }
+}
